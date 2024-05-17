51d9a78e29e667918d202a026c38d8312a1bd50a
Peeranat Srisuthangkul <peeranat.s@arise.tech>
2024-05-17 14:48:11 +0700

    add test case input 3

--- fizzbuzz_test.go
+++ fizzbuzz_test.go
@@ -9,10 +9,10 @@ func TestFizzBu(t *testing.T) {
 	t.Run("input 1 expected 1", func(t *testing.T) {
 		// Arrange
 		expected := "1"
-
+		input := "1"
 		// Act
 	
-		output := fizzBuzz(expected)
+		output := fizzBuzz(input)
 
 		// Assert
 
@@ -22,10 +22,22 @@ func TestFizzBu(t *testing.T) {
 	t.Run("input 2 expected 2", func(t *testing.T) {
 		// Arrange 
 		expected := "2"
-
+		input := "2"
 		// Act
 
-		output := fizzBuzz(expected)
+		output := fizzBuzz(input)
+
+		// Assert
+		assert.Equal(t, expected, output)
+	})
+
+	t.Run("input 3 expected 2", func(t *testing.T) {
+		// Arrange 
+		expected := "Fizz"
+		input := "3"
+		// Act
+
+		output := fizzBuzz(input)
 
 		// Assert
 		assert.Equal(t, expected, output)
