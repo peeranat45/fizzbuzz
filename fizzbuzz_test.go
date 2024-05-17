e2338fce78a9ff74ec014a4c88aae1d0eac0e6e1
Peeranat Srisuthangkul <peeranat.s@arise.tech>
2024-05-17 14:53:25 +0700

    add testcase 9

--- fizzbuzz_test.go
+++ fizzbuzz_test.go
@@ -78,6 +78,20 @@ func TestFizzBu(t *testing.T) {
 		// Assert
 		assert.Equal(t, output, expected)
 	})
+
+	t.Run("input 9 expected Fizz", func(t *testing.T) {
+		// Arrange
+		expected := "Fizz"
+		input := "9"
+
+		// Act
+		output := fizzBuzz(input)
+
+		// Assert
+		assert.Equal(t, output, expected)
+	})
+
+
 	
 
 }
