849b721a241f7d20e7a1a5f2f68823adde9d2ce4
Peeranat Srisuthangkul <peeranat.s@arise.tech>
2024-05-19 22:36:47 +0700

    testcase 5 pass

--- fizzbuzz.go
+++ fizzbuzz.go
@@ -5,8 +5,16 @@ import (
 )
 func fizzBuzz(input int) string {
 
-	if input == 3 {
-		return "Fizz"
+	mapFizz := make(map[bool]string, 2)
+	mapFizz[true] = "Fizz"
+	mapFizz[false] = ""
+
+	mapBuzz := make(map[bool]string, 2)
+	mapBuzz[true] = "Buzz"
+	mapFizz[false] = ""
+
+	if input % 3 == 0 || input % 5 == 0 {
+		return mapFizz[input%3 == 0] + mapBuzz[input%5 == 0]
 	}
 	return strconv.Itoa(input)
 }
