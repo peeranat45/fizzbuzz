67b0b28f22d38cf623410e4f0acde8ced5630d90
Peeranat Srisuthangkul <peeranat.s@arise.tech>
2024-05-23 15:28:33 +0700

    testcase 30 pass

--- fizzbuzz_test.go
+++ fizzbuzz_test.go
@@ -126,4 +126,28 @@ func TestFizzBu(t *testing.T) {
 		assert.Equal(t, output, expected)
 	})
 
+	t.Run("input 15 expected FizzBuzz", func(t *testing.T) {
+		// Arrange
+		expected := "FizzBuzz"
+		input := "15"
+
+		// Act
+		output := fizzBuzz(input)
+
+		// Assert
+		assert.Equal(t, output, expected)
+	})
+
+	t.Run("input 30 expected FizzBuzz", func(t *testing.T) {
+		// Arrange
+		expected := "FizzBuzz"
+		input := "30"
+
+		// Act
+		output := fizzBuzz(input)
+
+		// Assert
+		assert.Equal(t, output, expected)
+	})
+
 }
