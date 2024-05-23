6346adfd5ca88253208eb24e48478152d34fab98
Peeranat Srisuthangkul <peeranat.s@arise.tech>
2024-05-23 15:18:47 +0700

    testcase 3 pass

--- fizzbuzz.go
+++ fizzbuzz.go
@@ -1,6 +1,18 @@
 package main
 
+import "strconv"
+
+
 
 func fizzBuzz(input string) string {
-	return input
+
+	inputInt, err := strconv.Atoi(input)
+	if err != nil {
+		return input
+	}
+	fizzMap := make(map[bool]string, 2)
+
+	fizzMap[true] = "Fizz"
+	fizzMap[false] = input
+	return fizzMap[inputInt % 3 == 0]
 }
